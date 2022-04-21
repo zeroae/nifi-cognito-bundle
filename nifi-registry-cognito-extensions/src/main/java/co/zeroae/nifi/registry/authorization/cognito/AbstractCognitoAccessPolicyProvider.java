7998c9ac5c3736e7d5ac4ea637392728c225f8cb
Patrick Sodré <psodre@gmail.com>
2022-04-21 10:23:16 -0400

    Add naive implementation of Registry AccessPolicy Provider
     - Does not include caching

---
+++ nifi-registry-cognito-extensions/src/main/java/co/zeroae/nifi/registry/authorization/cognito/AbstractCognitoAccessPolicyProvider.java
@@ -0,0 +1,170 @@
+package co.zeroae.nifi.registry.authorization.cognito;
+
+import org.apache.nifi.registry.properties.NiFiRegistryProperties;
+import org.apache.nifi.registry.properties.util.IdentityMapping;
+import org.apache.nifi.registry.properties.util.IdentityMappingUtil;
+import org.apache.nifi.registry.security.authorization.*;
+import org.apache.nifi.registry.security.authorization.annotation.AuthorizerContext;
+import org.apache.nifi.registry.security.exception.SecurityProviderCreationException;
+import org.apache.nifi.registry.security.exception.SecurityProviderDestructionException;
+import org.apache.nifi.registry.util.PropertyValue;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.utils.StringUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public abstract class AbstractCognitoAccessPolicyProvider implements AccessPolicyProvider {
+    public static final String PROP_AWS_CREDENTIALS_FILE = "AWS Credentials File";
+    public static final String PROP_USER_POOL_ID = "User Pool";
+    public static final String PROP_USER_GROUP_PROVIDER = "User Group Provider";
+    public static final String PROP_INITIAL_ADMIN_IDENTITY = "Initial Admin Identity";
+    public static final String PROP_NODE_GROUP_NAME = "Node Group";
+
+    public static final int MAX_PAGE_SIZE = 60;
+
+    public static final String ACCESS_POLICY_GROUP_PREFIX =
+            AbstractCognitoUserGroupProvider.EXCLUDE_GROUP_PREFIX + "nfr:";
+
+    static final String ACCESS_KEY_PROPS_NAME = "aws.access.key.id";
+    static final String SECRET_KEY_PROPS_NAME = "aws.secret.access.key";
+
+    NiFiRegistryProperties properties;
+    UserGroupProviderLookup userGroupProviderLookup;
+
+    UserGroupProvider userGroupProvider;
+
+    CognitoIdentityProviderClient cognitoClient;
+    String userPoolId;
+    int pageSize;
+
+    User initialAdmin;
+    Group initialNodeGroup;
+
+    String policyGroupPrefix;
+
+    @AuthorizerContext
+    public void setup(NiFiRegistryProperties properties) { this.properties = properties; }
+
+    @Override
+    public UserGroupProvider getUserGroupProvider() {
+        return userGroupProvider;
+    }
+
+    @Override
+    public void initialize(AccessPolicyProviderInitializationContext initializationContext) throws SecurityProviderCreationException {
+        userGroupProviderLookup = initializationContext.getUserGroupProviderLookup();
+    }
+
+    @Override
+    public void onConfigured(AuthorizerConfigurationContext configurationContext) throws SecurityProviderCreationException {
+        final PropertyValue userGroupProviderIdentifier = configurationContext.getProperty(PROP_USER_GROUP_PROVIDER);
+        if (!userGroupProviderIdentifier.isSet())
+            throw new SecurityProviderCreationException("The user group provider must be specified");
+
+        userGroupProvider = userGroupProviderLookup.getUserGroupProvider(userGroupProviderIdentifier.getValue());
+        if (userGroupProvider == null)
+            throw new SecurityProviderCreationException("Unable to locate user group provider with identifier " + userGroupProviderIdentifier.getValue());
+
+        pageSize = MAX_PAGE_SIZE;
+
+        userPoolId = getProperty(configurationContext, PROP_USER_POOL_ID, null);
+        if (userPoolId == null)
+            throw new SecurityProviderCreationException("User Pool must be valid.");
+
+        policyGroupPrefix = ACCESS_POLICY_GROUP_PREFIX + "";
+
+        try {
+            final String credentialsFile = getProperty(configurationContext, PROP_AWS_CREDENTIALS_FILE, null);
+            cognitoClient = configureClient(credentialsFile);
+        } catch (IOException e) {
+            throw new SecurityProviderCreationException(e.getMessage(), e);
+        }
+
+        // extract any new identities
+        final List<IdentityMapping> identityMappings = Collections.unmodifiableList(
+                IdentityMappingUtil.getIdentityMappings(properties));
+        final List<IdentityMapping> groupMappings = Collections.unmodifiableList(
+                IdentityMappingUtil.getGroupMappings(properties));
+
+        // get the value of the initial admin identity
+        final PropertyValue initialAdminIdentityProp = configurationContext.getProperty(PROP_INITIAL_ADMIN_IDENTITY);
+        final String initialAdminIdentity = initialAdminIdentityProp.isSet() ? IdentityMappingUtil.mapIdentity(initialAdminIdentityProp.getValue(), identityMappings) : null;
+        if (initialAdminIdentity != null)
+            initialAdmin = userGroupProvider.getUserByIdentity(initialAdminIdentity);
+
+        // extract any node identities
+        final PropertyValue initialNodeGroupProp = configurationContext.getProperty(PROP_NODE_GROUP_NAME);
+        final String initialNodeGroupIdentity = initialNodeGroupProp.isSet() ? IdentityMappingUtil.mapIdentity(initialNodeGroupProp.getValue(), groupMappings) : null;
+        if (initialNodeGroupIdentity != null)
+            initialNodeGroup = userGroupProvider.getGroups().stream()
+                    .filter(group -> group.getName().equals(initialNodeGroupIdentity))
+                    .findFirst()
+                    .orElse(null);
+    }
+
+    @Override
+    public void preDestruction() throws SecurityProviderDestructionException {
+        cognitoClient.close();
+        userPoolId = null;
+        pageSize = 0;
+    }
+
+    CognitoIdentityProviderClient configureClient(final String awsCredentialsFilename) throws IOException {
+        if (awsCredentialsFilename == null) {
+            return getDefaultClient();
+        }
+        final Properties properties = loadProperties(awsCredentialsFilename);
+        final String accessKey = properties.getProperty(ACCESS_KEY_PROPS_NAME);
+        final String secretKey = properties.getProperty(SECRET_KEY_PROPS_NAME);
+        final Region region = Region.of(userPoolId.substring(0, userPoolId.indexOf('_')));
+
+        AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        if (isNotBlank(accessKey) && isNotBlank(secretKey))
+            return CognitoIdentityProviderClient.builder()
+                    .region(region)
+                    .credentialsProvider(StaticCredentialsProvider.create(basicCredentials))
+                    .build();
+        else
+            return getDefaultClient();
+    }
+
+    private CognitoIdentityProviderClient getDefaultClient() {
+        return CognitoIdentityProviderClient.builder()
+                .region(Region.of(userPoolId.substring(0, userPoolId.indexOf('_'))))
+                .build();
+    }
+
+    private Properties loadProperties(final String propertiesFilename) throws IOException {
+        final Properties properties = new Properties();
+
+        try (final InputStream in = new FileInputStream(Paths.get(propertiesFilename).toFile())) {
+            properties.load(in);
+            return properties;
+        }
+    }
+
+    private static boolean isNotBlank(final String value) {
+        return value != null && !value.trim().equals("");
+    }
+
+    protected String getProperty(AuthorizerConfigurationContext authContext, String propertyName, String defaultValue) {
+        final PropertyValue property = authContext.getProperty(propertyName);
+        if (property != null && property.isSet()) {
+            final String value = property.getValue();
+            if (StringUtils.isNotBlank(value)) {
+                return value;
+            }
+        }
+        return defaultValue;
+    }
+
+}
---
+++ nifi-registry-cognito-extensions/src/main/java/co/zeroae/nifi/registry/authorization/cognito/CognitoNaiveAccessPolicyProvider.java
@@ -0,0 +1,293 @@
+package co.zeroae.nifi.registry.authorization.cognito;
+
+import org.apache.nifi.registry.security.authorization.*;
+import org.apache.nifi.registry.security.authorization.exception.AuthorizationAccessException;
+import org.apache.nifi.registry.security.exception.SecurityProviderCreationException;
+import org.apache.nifi.util.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
+
+import java.lang.UnsupportedOperationException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CognitoNaiveAccessPolicyProvider extends AbstractCognitoAccessPolicyProvider implements ConfigurableAccessPolicyProvider {
+    private final static Logger logger = LoggerFactory.getLogger(CognitoNaiveAccessPolicyProvider.class);
+
+
+    @Override
+    public void onConfigured(AuthorizerConfigurationContext configurationContext) throws SecurityProviderCreationException {
+        super.onConfigured(configurationContext);
+
+        // Ideally we would use this, but it is in the wrong .jar
+        // https://github.com/apache/nifi/blob/main/nifi-registry/nifi-registry-core/nifi-registry-framework/src/main/java/org/apache/nifi/registry/security/authorization/util/InitialPolicies.java
+        final List<RequestAction> read = Collections.singletonList(RequestAction.READ);
+        final List<RequestAction> all = Collections.unmodifiableList(Arrays.asList(
+                RequestAction.READ, RequestAction.WRITE, RequestAction.DELETE
+        ));
+        final Map<String, Map<String, List<RequestAction>>> identifierToPolicies = new HashMap<>();
+
+        if (initialNodeGroup != null) {
+            Map<String, List<RequestAction>> policies = new HashMap<String, List<RequestAction>>() {{
+                put("/buckets", read);
+                put("/proxy", all);
+            }};
+            identifierToPolicies.put(getGroupProxyUsername(initialNodeGroup.getIdentifier()), policies);
+        }
+
+        if (initialAdmin != null) {
+            Map<String, List<RequestAction>> policies = Collections.unmodifiableMap(new HashMap<String, List<RequestAction>>() {{
+                put("/tenants", all);
+                put("/policies", all);
+                put("/buckets", all);
+                put("/actuator", all);
+                put("/swagger", all);
+                put("/proxy", all);
+            }});
+            identifierToPolicies.put(initialAdmin.getIdentifier(), policies);
+        }
+
+        identifierToPolicies.forEach((principal, value) -> value.entrySet().stream()
+                .map(entry -> entry.getValue().stream()
+                        .map(action -> new AccessPolicy.Builder()
+                                .identifierGenerateRandom()
+                                .resource(entry.getKey())
+                                .action(action)
+                                .build()
+                        ).collect(Collectors.toList()))
+                .flatMap(Collection::stream)
+                .map(policy -> {
+                    final AccessPolicy rv = getAccessPolicy(policy.getResource(), policy.getAction());
+                    return rv == null ? addAccessPolicy(policy, false) : rv;
+                })
+                .forEach(policy -> addPrincipalToPolicy(principal, policy)));
+    }
+
+    @Override
+    public Set<AccessPolicy> getAccessPolicies() throws AuthorizationAccessException {
+        StopWatch watch = new StopWatch();
+        final ListGroupsRequest request = ListGroupsRequest.builder()
+                .userPoolId(userPoolId)
+                .limit(pageSize)
+                .build();
+        watch.start();
+        try {
+            final Set<AccessPolicy> rv = cognitoClient.listGroupsPaginator(request).groups().stream()
+                    .filter(group -> group.groupName().startsWith(policyGroupPrefix))
+                    .map(this::buildAccessPolicy)
+                    .collect(Collectors.toSet());
+            return Collections.unmodifiableSet(rv);
+        } catch (CognitoIdentityProviderException e){
+            throw new AuthorizationAccessException(e.getMessage(), e);
+        } finally {
+            watch.stop();
+            logger.debug("getAccessPolicies: " + watch.getDuration());
+        }
+    }
+
+    @Override
+    public AccessPolicy getAccessPolicy(String identifier) throws AuthorizationAccessException {
+        final Set<AccessPolicy> allPolicies = getAccessPolicies();
+        if (allPolicies == null)
+            return null;
+
+        return allPolicies.stream()
+                .filter(policy -> policy.getIdentifier().equals(identifier))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public AccessPolicy getAccessPolicy(String resource, RequestAction action) throws AuthorizationAccessException {
+        StopWatch watch = new StopWatch();
+        GetGroupRequest request = GetGroupRequest.builder()
+                .userPoolId(userPoolId)
+                .groupName(getGroupName(resource, action))
+                .build();
+        watch.start();
+        try {
+            GroupType group = cognitoClient.getGroup(request).group();
+            return buildAccessPolicy(group);
+        } catch (ResourceNotFoundException e) {
+            return null;
+        } catch (CognitoIdentityProviderException e) {
+            throw new AuthorizationAccessException(e.getMessage(), e);
+        } finally {
+            watch.stop();
+            logger.debug("getAccessPolicy: " + watch.getDuration());
+        }
+    }
+
+    protected AccessPolicy buildAccessPolicy(GroupType group) {
+        logger.debug("Building AccessPolicy from " + group);
+        Map.Entry<String, RequestAction> resourceAndAction = getResourceAndAction(group.groupName());
+        AccessPolicy.Builder accessPolicyBuilder = new AccessPolicy.Builder()
+                .identifier(group.description())
+                .resource(resourceAndAction.getKey())
+                .action(resourceAndAction.getValue());
+        getPrincipalsInPolicy(group).forEach(principal -> {
+            if (principal.startsWith(AbstractCognitoUserGroupProvider.GROUP_PROXY_USER_PREFIX))
+                accessPolicyBuilder.addGroup(principal.substring(AbstractCognitoUserGroupProvider.GROUP_PROXY_USER_PREFIX.length()));
+            else
+                accessPolicyBuilder.addUser(principal);
+        });
+        return accessPolicyBuilder.build();
+    }
+
+
+    protected Set<String> getPrincipalsInPolicy(GroupType groupType) throws AuthorizationAccessException {
+        StopWatch watch = new StopWatch();
+        final ListUsersInGroupRequest listUsersInGroupRequest = ListUsersInGroupRequest.builder()
+                .userPoolId(userPoolId)
+                .limit(pageSize)
+                .groupName(groupType.groupName())
+                .build();
+        watch.start();
+        try {
+            return cognitoClient.listUsersInGroupPaginator(listUsersInGroupRequest)
+                    .users()
+                    .stream()
+                    .map(UserType::username)
+                    .collect(Collectors.toSet());
+        } catch (CognitoIdentityProviderException e) {
+            throw new AuthorizationAccessException(e.getMessage(), e);
+        } finally {
+            watch.stop();
+            logger.debug("getPrincipalsInPolicy: " + watch.getDuration());
+        }
+    }
+
+    protected String getGroupName(AccessPolicy accessPolicy) {
+        return getGroupName(accessPolicy.getResource(), accessPolicy.getAction());
+    }
+
+    protected String getGroupName(String resource, RequestAction action) {
+        return String.join(":", policyGroupPrefix, action.toString(), resource);
+    }
+
+    protected Map.Entry<String, RequestAction> getResourceAndAction(String groupName) {
+        // <acl>:<nfc|nfr>:[cluster-id|registry-id]:<action>:<resource>
+        final String[] acl = groupName.split(":", 5);
+        return new AbstractMap.SimpleEntry<>(acl[4], RequestAction.valueOfValue(acl[3]));
+    }
+
+    @Override
+    public String getFingerprint() throws AuthorizationAccessException {
+        return userPoolId;
+    }
+
+    @Override
+    public void inheritFingerprint(String fingerprint) throws AuthorizationAccessException {
+        // nothing to do, userPoolIds are already the same already!
+        checkInheritability(fingerprint);
+    }
+
+    @Override
+    public void checkInheritability(String proposedFingerprint) throws AuthorizationAccessException {
+        if (!proposedFingerprint.equals(userPoolId)) {
+            throw new AuthorizationAccessException("Inheritance is not supported for different userPoolIds");
+        }
+    }
+
+    @Override
+    public AccessPolicy addAccessPolicy(AccessPolicy accessPolicy) throws AuthorizationAccessException {
+        return addAccessPolicy(accessPolicy, true);
+    }
+
+    private AccessPolicy addAccessPolicy(AccessPolicy accessPolicy, boolean setPrincipals) {
+        CreateGroupRequest request = CreateGroupRequest.builder()
+                .userPoolId(userPoolId)
+                .description(accessPolicy.getIdentifier())
+                .groupName(getGroupName(accessPolicy))
+                .build();
+        try {
+            cognitoClient.createGroup(request);
+        } catch (GroupExistsException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } catch (CognitoIdentityProviderException e) {
+            logger.error("Error creating policy: " + accessPolicy);
+            throw new AuthorizationAccessException(e.getMessage(), e);
+        }
+        return setPrincipals ? updateAccessPolicy(accessPolicy) : accessPolicy;
+    }
+
+
+    @Override
+    public AccessPolicy updateAccessPolicy(AccessPolicy accessPolicy) throws AuthorizationAccessException {
+        AccessPolicy current = getAccessPolicy(accessPolicy.getResource(), accessPolicy.getAction());
+
+        if (!(current.getIdentifier().equals(accessPolicy.getIdentifier())))
+            throw new UnsupportedOperationException("Cognito can't change policy identifier.");
+
+        final Set<String> usersToAdd = new HashSet<>(accessPolicy.getUsers());
+        usersToAdd.removeAll(current.getUsers());
+        usersToAdd.forEach(user -> addPrincipalToPolicy(user, current));
+
+        final Set<String> usersToRemove = new HashSet<>(current.getUsers());
+        usersToRemove.removeAll(accessPolicy.getUsers());
+        usersToRemove.forEach(user -> removePrincipalFromPolicy(user, current));
+
+        final Set<String> groupsToAdd = new HashSet<>(accessPolicy.getGroups());
+        groupsToAdd.removeAll(current.getGroups());
+        groupsToAdd.forEach(group -> addPrincipalToPolicy(getGroupProxyUsername(group), current));
+
+        final Set<String> groupsToRemove = new HashSet<>(current.getGroups());
+        groupsToRemove.removeAll(accessPolicy.getGroups());
+        groupsToRemove.forEach(group -> removePrincipalFromPolicy(getGroupProxyUsername(group), current));
+
+        return getAccessPolicy(accessPolicy.getResource(), accessPolicy.getAction());
+    }
+
+    private String getGroupProxyUsername(String groupIdentifier) {
+        return groupIdentifier.startsWith(AbstractCognitoUserGroupProvider.GROUP_PROXY_USER_PREFIX) ? groupIdentifier : AbstractCognitoUserGroupProvider.GROUP_PROXY_USER_PREFIX + groupIdentifier;
+    }
+
+    @Override
+    public AccessPolicy deleteAccessPolicy(AccessPolicy accessPolicy) throws AuthorizationAccessException {
+        DeleteGroupRequest request = DeleteGroupRequest.builder()
+                .userPoolId(userPoolId)
+                .groupName(getGroupName(accessPolicy))
+                .build();
+        try {
+            cognitoClient.deleteGroup(request);
+        } catch (final ResourceNotFoundException e) {
+            return null;
+        } catch (final CognitoIdentityProviderException e) {
+            throw new AuthorizationAccessException("Error deleting policy: " + accessPolicy, e);
+        }
+        return accessPolicy;
+    }
+
+    protected void removePrincipalFromPolicy(String principalIdentifier, AccessPolicy policy) {
+        try {
+            cognitoClient.adminRemoveUserFromGroup(AdminRemoveUserFromGroupRequest.builder()
+                    .userPoolId(userPoolId)
+                    .username(principalIdentifier)
+                    .groupName(getGroupName(policy))
+                    .build());
+        } catch (UserNotFoundException e) {
+            logger.warn(String.format("Error removing '%s' from policy '%s'. User not found in Cognito.",
+                    principalIdentifier, policy));
+        } catch (CognitoIdentityProviderException e) {
+            logger.error(String.format("Error removing user %s from policy %s", principalIdentifier, policy), e);
+            throw new AuthorizationAccessException(e.getMessage(), e);
+        }
+    }
+
+    protected void addPrincipalToPolicy(String principalIdentifier, AccessPolicy policy) {
+        try {
+            cognitoClient.adminAddUserToGroup(AdminAddUserToGroupRequest.builder()
+                    .userPoolId(userPoolId)
+                    .username(principalIdentifier)
+                    .groupName(getGroupName(policy))
+                    .build());
+        } catch (UserNotFoundException e) {
+            logger.warn(String.format("Error adding principal '%s' to policy '%s'. User was not found in Cognito.",
+                    principalIdentifier, policy));
+        } catch (CognitoIdentityProviderException e) {
+            logger.error(String.format("Error adding principal '%s' to group '%s'", principalIdentifier, policy), e);
+            throw new AuthorizationAccessException(e.getMessage(), e);
+        }
+    }
+}
